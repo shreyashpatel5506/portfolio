5804d09023e9080361395c5a81945ab8bfcc1709
Shreyash Patel <shreyashpatel5506@gmail.com>
2025-10-09 18:00:57 +0530

    change in the button ui

--- app/components/Home.js
+++ app/components/Home.js
@@ -98,7 +98,7 @@ const Home = () => {
             <div className='flex flex-row  items-center justify-between gap-12'>
               <button
                 type="submit"
-                className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg  text-blue-700 bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-blue-500 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+                className="ghost-button flex flex-row items-center gap-5"
               >
                 Projects
                 <svg
@@ -114,7 +114,7 @@ const Home = () => {
               </button>
               <button
                 type="submit"
-                className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg  text-blue-700 bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-blue-500 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+                className="ghost-button  flex flex-row items-center gap-5"
               >
                 Download Resume
                 <svg
--- app/components/Skill.js
+++ app/components/Skill.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Frontend from './Skill/Frontend';
 
 const Skill = () => {
     return (
-        <div className='grid grid-cols-2'></div>
+        <div className='grid grid-cols-2  bg-[#1A1A2E] border-t-2    border-[#30A585] pt-10 pl-7 pr-7'>
+            {/* left side */}
+            <div className='width-[30%] flex flex-col gap-10 items-baseline justify-center' >
+                <button className='ghost-button' >Frontend</button>
+            </div>
+            {/* RightSide */}
+            <div className='width-[70%]'>
+                <Frontend />
+            </div>
+        </div>
     )
 }
 
