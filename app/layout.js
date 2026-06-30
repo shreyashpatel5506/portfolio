672822b4fb40cdba0c3e9a68ff01e90debc92d66
Shreyash Patel <shreyashpatel5506@gmail.com>
2026-06-30 12:27:10 +0530

    changed

--- app/layout.js
+++ app/layout.js
@@ -36,7 +36,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      suppressHydrationWarning
+      suppressHydrationWarning={true}
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
